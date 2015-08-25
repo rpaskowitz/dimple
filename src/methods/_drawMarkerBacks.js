c1b706210a2089878853ab4e1c48241ec98bb8ff
Robert Paskowitz <robert.paskowitz@upsight.com>
2015-08-25 20:22:23 +0000

    Caching for markers

--- src/methods/_drawMarkerBacks.js
+++ src/methods/_drawMarkerBacks.js
@@ -12,52 +12,57 @@
             } else {
                 markerBacks = series._markerBacks[lineDataRow.keyString].data(lineDataRow.markerData, function (d) { return d.key; });
             }
-
-            // Add
-            if (lineShape.nextSibling && lineShape.nextSibling.id) {
-                shapes = markerBacks.enter().insert("circle", '#' + lineShape.nextSibling.id);
-            } else {
-                shapes = markerBacks.enter().append("circle");
-            }
-            shapes
-                .attr("id", function (d) { return dimple._createClass([d.key + " Marker Back"]); })
-                .attr("class", function (d) {
-                    var fields = [];
-                    if (series.x._hasCategories()) {
-                        fields = fields.concat(d.xField);
-                    }
-                    if (series.y._hasCategories()) {
-                        fields = fields.concat(d.yField);
-                    }
-                    return dimple._createClass(fields) + " " + markerBackClasses.join(" ");
-                })
-                .attr("cx", function (d) { return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._previousOrigin); })
-                .attr("cy", function (d) { return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._previousOrigin); })
-                .attr("r", 0)
-                .attr("fill", "white")
-                .attr("stroke", "none");
-
-            // Update
-            chart._handleTransition(markerBacks, duration, chart)
-                .attr("cx", function (d) { return dimple._helpers.cx(d, chart, series); })
-                .attr("cy", function (d) { return dimple._helpers.cy(d, chart, series); })
-                .attr("r", 2 + series.lineWeight);
-
-            // Remove
-            rem = chart._handleTransition(markerBacks.exit(), duration, chart)
-                .attr("cx", function (d) { return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._origin); })
-                .attr("cy", function (d) { return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._origin); })
-                .attr("r", 0);
-
-            // Run after transition methods
-            if (duration === 0) {
-                rem.remove();
-            } else {
-                rem.each("end", function () {
-                    d3.select(this).remove();
+            fastdom.read(function() {
+                shapes.each(function(d) {
+                    dimple._helpers.buildCache(d, chart, series);
                 });
-            }
+            });
+            fastdom.write(function() {
+                // Add
+                if (lineShape.nextSibling && lineShape.nextSibling.id) {
+                    shapes = markerBacks.enter().insert("circle", '#' + lineShape.nextSibling.id);
+                } else {
+                    shapes = markerBacks.enter().append("circle");
+                }
+                shapes
+                    .attr("id", function (d) { return dimple._createClass([d.key + " Marker Back"]); })
+                    .attr("class", function (d) {
+                        var fields = [];
+                        if (series.x._hasCategories()) {
+                            fields = fields.concat(d.xField);
+                        }
+                        if (series.y._hasCategories()) {
+                            fields = fields.concat(d.yField);
+                        }
+                        return dimple._createClass(fields) + " " + markerBackClasses.join(" ");
+                    })
+                    .attr("cx", function (d) { return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._previousOrigin); })
+                    .attr("cy", function (d) { return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._previousOrigin); })
+                    .attr("r", 0)
+                    .attr("fill", "white")
+                    .attr("stroke", "none");
 
+                // Update
+                chart._handleTransition(markerBacks, duration, chart)
+                    .attr("cx", function (d) { return dimple._helpers.cx(d, chart, series); })
+                    .attr("cy", function (d) { return dimple._helpers.cy(d, chart, series); })
+                    .attr("r", 2 + series.lineWeight);
+
+                // Remove
+                rem = chart._handleTransition(markerBacks.exit(), duration, chart)
+                    .attr("cx", function (d) { return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._origin); })
+                    .attr("cy", function (d) { return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._origin); })
+                    .attr("r", 0);
+
+                // Run after transition methods
+                if (duration === 0) {
+                    rem.remove();
+                } else {
+                    rem.each("end", function () {
+                        d3.select(this).remove();
+                    });
+                }
+            });
             if (series._markerBacks === undefined || series._markerBacks === null) {
                 series._markerBacks = {};
             }
--- src/methods/_drawMarkers.js
+++ src/methods/_drawMarkers.js
@@ -15,81 +15,91 @@
                 return d.key;
             });
         }
-        // Add
-        if (lineShape.nextSibling && lineShape.nextSibling.id) {
-            shapes = markers.enter().insert("circle", '#' + lineShape.nextSibling.id);
-        } else {
-            shapes = markers.enter().append("circle");
-        }
-        shapes
-            .attr("id", function (d) {
-                return dimple._createClass([d.key + " Marker"]);
-            })
-            .attr("class", function (d) {
-                var fields = [],
-                    css = chart.getClass(d.aggField.length > 0 ? d.aggField[d.aggField.length - 1] : "All");
-                if (series.x._hasCategories()) {
-                    fields = fields.concat(d.xField);
-                }
-                if (series.y._hasCategories()) {
-                    fields = fields.concat(d.yField);
-                }
-                return dimple._createClass(fields) + " " + markerClasses.join(" ") + " " + chart.customClassList.lineMarker + " " + css;
-            })
-            .on("mouseover", function (e) {
-                enterEventHandler(e, this, chart, series);
-            })
-            .on("mouseleave", function (e) {
-                leaveEventHandler(e, this, chart, series);
-            })
-            .attr("cx", function (d) {
-                return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._previousOrigin);
-            })
-            .attr("cy", function (d) {
-                return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._previousOrigin);
-            })
-            .attr("r", 0)
-            .attr("opacity", (series.lineMarkers || lineDataRow.data.length < 2 ? lineDataRow.color.opacity : 0))
-            .call(function () {
-                if (!chart.noFormats) {
-                    this.attr("fill", "white")
-                        .style("stroke-width", series.lineWeight)
-                        .attr("stroke", function (d) {
-                            return (useGradient ? dimple._helpers.fill(d, chart, series) : lineDataRow.color.stroke);
-                        });
-                }
-            });
 
-        // Update
-        chart._handleTransition(markers, duration, chart)
-            .attr("cx", function (d) { return dimple._helpers.cx(d, chart, series); })
-            .attr("cy", function (d) { return dimple._helpers.cy(d, chart, series); })
-            .attr("r", 2 + series.lineWeight)
-            .attr("opacity", (series.lineMarkers || lineDataRow.data.length < 2 ? lineDataRow.color.opacity : 0))
-            .call(function () {
-                if (!chart.noFormats) {
-                    this.attr("fill", "white")
-                        .style("stroke-width", series.lineWeight)
-                        .attr("stroke", function (d) {
-                            return (useGradient ? dimple._helpers.fill(d, chart, series) : lineDataRow.color.stroke);
-                        });
-                }
-            });
+        fastdom.read(function() {
+            if (shapes !== undefined) {
+                shapes.each(function(d) {
+                    dimple._helpers.buildCache(d, chart, series);
+                });
+            }
+        });
+        fastdom.write(function() {
+            // Add
+            if (lineShape.nextSibling && lineShape.nextSibling.id) {
+                shapes = markers.enter().insert("circle", '#' + lineShape.nextSibling.id);
+            } else {
+                shapes = markers.enter().append("circle");
+            }
+            shapes
+                .attr("id", function (d) {
+                    return dimple._createClass([d.key + " Marker"]);
+                })
+                .attr("class", function (d) {
+                    var fields = [],
+                        css = chart.getClass(d.aggField.length > 0 ? d.aggField[d.aggField.length - 1] : "All");
+                    if (series.x._hasCategories()) {
+                        fields = fields.concat(d.xField);
+                    }
+                    if (series.y._hasCategories()) {
+                        fields = fields.concat(d.yField);
+                    }
+                    return dimple._createClass(fields) + " " + markerClasses.join(" ") + " " + chart.customClassList.lineMarker + " " + css;
+                })
+                .on("mouseover", function (e) {
+                    enterEventHandler(e, this, chart, series);
+                })
+                .on("mouseleave", function (e) {
+                    leaveEventHandler(e, this, chart, series);
+                })
+                .attr("cx", function (d) {
+                    return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._previousOrigin);
+                })
+                .attr("cy", function (d) {
+                    return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._previousOrigin);
+                })
+                .attr("r", 0)
+                .attr("opacity", (series.lineMarkers || lineDataRow.data.length < 2 ? lineDataRow.color.opacity : 0))
+                .call(function () {
+                    if (!chart.noFormats) {
+                        this.attr("fill", "white")
+                            .style("stroke-width", series.lineWeight)
+                            .attr("stroke", function (d) {
+                                return (useGradient ? dimple._helpers.fill(d, chart, series) : lineDataRow.color.stroke);
+                            });
+                    }
+                });
 
-        // Remove
-        rem = chart._handleTransition(markers.exit(), duration, chart)
-            .attr("cx", function (d) { return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._origin); })
-            .attr("cy", function (d) { return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._origin); })
-            .attr("r", 0);
+            // Update
+            chart._handleTransition(markers, duration, chart)
+                .attr("cx", function (d) { return dimple._helpers.cx(d, chart, series); })
+                .attr("cy", function (d) { return dimple._helpers.cy(d, chart, series); })
+                .attr("r", 2 + series.lineWeight)
+                .attr("opacity", (series.lineMarkers || lineDataRow.data.length < 2 ? lineDataRow.color.opacity : 0))
+                .call(function () {
+                    if (!chart.noFormats) {
+                        this.attr("fill", "white")
+                            .style("stroke-width", series.lineWeight)
+                            .attr("stroke", function (d) {
+                                return (useGradient ? dimple._helpers.fill(d, chart, series) : lineDataRow.color.stroke);
+                            });
+                    }
+                });
 
-        // Run after transition methods
-        if (duration === 0) {
-            rem.remove();
-        } else {
-            rem.each("end", function () {
-                d3.select(this).remove();
-            });
-        }
+            // Remove
+            rem = chart._handleTransition(markers.exit(), duration, chart)
+                .attr("cx", function (d) { return (series.x._hasCategories() ? dimple._helpers.cx(d, chart, series) : series.x._origin); })
+                .attr("cy", function (d) { return (series.y._hasCategories() ? dimple._helpers.cy(d, chart, series) : series.y._origin); })
+                .attr("r", 0);
+
+            // Run after transition methods
+            if (duration === 0) {
+                rem.remove();
+            } else {
+                rem.each("end", function () {
+                    d3.select(this).remove();
+                });
+            }
+        });
 
         if (series._markers === undefined || series._markers === null) {
             series._markers = {};
